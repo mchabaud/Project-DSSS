10f0aaa051dc697c3c22c77f94f8e033c597d45a
mchabaud <72380439+mchabaud@users.noreply.github.com>
2022-03-13 12:22:31 +0100

    Update import_data.R

--- import_data.R
+++ import_data.R
@@ -1,5 +1,7 @@
 #répertoires
 mortalite_path <- "~/ENSAE/3A/Projet_DSSS/Données/Mortalité"
+APL_path <- "~/ENSAE/3A/Projet_DSSS/Données/APL_densité_médicale/APL"
+covariable_path <- "~/ENSAE/3A/Projet_DSSS/Données/Covariables"
 
 #décès
 
@@ -82,17 +84,148 @@ deces <- lapply(deces, clean_date)
 
 deces_15_19 <- bind_rows(deces) %>% distinct(.keep_all = T)
 
-A <- deces_15_19 %>% group_by(deces_code_lieu) %>% summarise(n = n())
+#A <- deces_15_19 %>% group_by(deces_code_lieu) %>% summarise(n = n())
 
 any(is.na(deces_15_19$naissance_date_complete))
 any(is.na(deces_15_19$deces_date_complete))
 
 
+#âge moyen décès par département
+
+deces_15_19 %<>% mutate(age = round(as.numeric((deces_date_complete - naissance_date_complete)/365),1))
+
+# ggplot(deces_15_19, aes(x=age)) + 
+#   geom_density()
+# 
+# quantile(deces_15_19$age, probs = c(0,0.01,0.25,0.5,0.75,0.9,0.99,0.999))
+# 
+# 
+# nrow(deces_15_19 %>% filter(age == 0))
+# deces_15_19 %>% group_by(sexe) %>% summarise(age_moyen = mean(age))
+
+table_commune <- read_csv("~/ENSAE/3A/Projet_DSSS/commune2021.csv") %>%
+  distinct(COM, .keep_all = T) #37122 communes/arrondissements municipaux
+
+deces_15_19 %<>% distinct(nom, sexe, naissance_date_complete, naissance_code_lieu, deces_date_complete, .keep_all = T) %>%
+  left_join(table_commune, by = c('deces_code_lieu' = 'COM')) %>%
+  mutate(annee_deces = year(deces_date_complete))
+
+communes_inconnues <- deces_15_19 %>% filter(is.na(NCC)) %>% group_by(deces_code_lieu) %>%
+  summarise(n = n()) %>% filter(!grepl("^9[7-9]", deces_code_lieu)) #111 communes
+sum(communes_inconnues$n) #4394 décès
+
+A <- deces_15_19 %>% group_by(annee_deces, DEP) %>% summarise(n = n(), age = mean(age)) %>%
+  filter(annee_deces > 2014) %>%
+  arrange(age, .by_group = T)
+
+#deces_15_19 %>% filter(annee_deces == 2015) %>% distinct(deces_code_lieu)
 
 
+#APL
 
-#base equipements
+files_list <- list.files(sprintf('%s', APL_path), pattern = '^APL_mg_[0-9]+.xlsx', full.names = T)
+APL <- lapply(files_list, read_excel)
+for (i in 1:5){
+  APL[[i]] %<>% mutate(annee = 2014 + i)
+}
 
-data <- read_csv2(file.choose())
-A <- data %>% distinct(UU2020, .keep_all = T)
-A <- data %>% filter(TYPEQU == "D201")
+B <- deces_15_19 %>% filter(!(deces_code_lieu %chin% APL$`Code commune`)) #48000 obs, plupart à l'étranger
+
+
+deces_15 <- deces_15_19 %>% select(1:12) %>%
+  mutate(annee_deces = year(deces_date_complete)) %>%
+  filter(annee_deces == 2015) %>%
+  group_by(deces_code_lieu) %>%
+  summarise(age_moy_deces = mean(age))
+
+deces_15 %<>% left_join(APL, by = c('deces_code_lieu' = 'Code commune'))
+
+deces_15 %<>% rename(APL = `APL aux médecins généralistes (sans borne d'âge)`) %>%
+  filter(!is.na(`Population totale`))
+
+model <- lm(age_moy_deces ~ APL, data = deces_15)
+summary(model)
+
+
+## 2018 ##
+
+#cross-section avec seulement APL
+
+deces_18 <- deces_15_19 %>%
+  filter(annee_deces == 2018) %>%
+  group_by(deces_code_lieu) %>%
+  summarise(nb_morts = n(), age_moy_deces = mean(age))
+
+APL_18 <- APL[[4]]
+
+deces_18 %>% filter(!(deces_code_lieu %in% APL_18$`Code commune`)) %>% view() #communes déléguées ou à l'étranger
+
+deces_18 %<>% left_join(APL_18, by = c('deces_code_lieu' = 'Code commune')) %>%
+  rename(APL = `APL aux médecins généralistes (sans borne d'âge)`)
+
+deces_18 %<>% rename(APL = `APL aux médecins généralistes (sans borne d'âge)`)
+
+model <- lm(age_moy_deces ~ APL, data = deces_18)
+summary(model)
+
+
+#cross-section avec APL + covariables
+
+#structure par âge, sexe et PCS
+
+recensement <- read_excel(sprintf('%s/population/base-cc-evol-struct-pop-2018.xlsx', covariable_path), sheet = "COM_2018", skip = 5)
+arrdt <- read_excel(sprintf('%s/population/base-cc-evol-struct-pop-2018.xlsx', covariable_path), sheet = "ARM_2018", skip = 5)
+recensement %<>% bind_rows(arrdt)
+
+var_sexe_age <- c("P18_H0014", "P18_H1529", "P18_H3044", "P18_H4559", "P18_H6074", "P18_H7589",
+         "P18_H90P", "P18_F0014", "P18_F1529", "P18_F3044", "P18_F4559", "P18_F6074",
+         "P18_F7589", "P18_F90P")
+
+recensement %<>% mutate(across(var_sexe_age, ~./P18_POP, .names = "{col}_prop"))
+
+var_PCS <- c("C18_POP15P_CS1", "C18_POP15P_CS2", "C18_POP15P_CS3", "C18_POP15P_CS4",
+         "C18_POP15P_CS5", "C18_POP15P_CS6", "C18_POP15P_CS7", "C18_POP15P_CS8")
+
+recensement %<>% mutate(across(var_PCS, ~./C18_POP15P, .names = "{col}_prop"))
+
+recensement %<>% relocate(c(1:6, C18_POP15P, 109:130,))
+
+taille_menage <- read_csv2(sprintf('%s/population/taille_moyenne_menage_2018.csv', covariable_path))
+taille_menage <- taille_menage[-c(1:2),c(1,3)]
+names(taille_menage) <- c("CODGEO", "Taille_menage")
+taille_menage %<>% mutate(Taille_menage = as.numeric(Taille_menage))
+recensement %<>% left_join(taille_menage, by = 'CODGEO')
+
+
+#niveau de vie médian
+
+filosofi <- read_excel(sprintf('%s/Filosofi/base-cc-filosofi-2018-geo2021.xlsx', covariable_path), sheet = "COM", skip = 5)
+filosofi %<>% select(CODGEO, MED18)
+recensement %<>% left_join(filosofi, by = 'CODGEO')
+
+#régression multiple
+
+deces_18 %<>% left_join(recensement, by = c('deces_code_lieu' = 'CODGEO'))
+
+deces_18 %<>% rename(APL_62 = `APL aux médecins généralistes de moins de 62 ans`) %>%
+  mutate(log_nb_morts = log(nb_morts))
+
+model <- lm(age_moy_deces ~ APL + P18_H0014_prop + P18_H1529_prop +
+            P18_H4559_prop + P18_H6074_prop + P18_H7589_prop + P18_H90P_prop + P18_F0014_prop +
+            P18_F1529_prop + P18_F3044_prop + P18_F4559_prop + P18_F6074_prop + P18_F7589_prop +
+            P18_F90P_prop + C18_POP15P_CS1_prop + C18_POP15P_CS2_prop + C18_POP15P_CS3_prop +
+            C18_POP15P_CS4_prop + C18_POP15P_CS5_prop + C18_POP15P_CS6_prop + C18_POP15P_CS7_prop + 
+            as.numeric(Taille_menage) + MED18, data = deces_18 %>% filter(nb_morts > 3))
+summary(model)
+
+model <- lm(log_nb_morts ~ APL + `Population totale` + P18_H0014_prop + P18_H1529_prop +
+              P18_H4559_prop + P18_H6074_prop + P18_H7589_prop + P18_H90P_prop + P18_F0014_prop +
+              P18_F1529_prop + P18_F3044_prop + P18_F4559_prop + P18_F6074_prop + P18_F7589_prop +
+              P18_F90P_prop + C18_POP15P_CS1_prop + C18_POP15P_CS2_prop + C18_POP15P_CS3_prop +
+              C18_POP15P_CS4_prop + C18_POP15P_CS5_prop + C18_POP15P_CS6_prop + C18_POP15P_CS7_prop + 
+              as.numeric(Taille_menage) + MED18, data = deces_18 %>% filter(nb_morts > 5))
+summary(model)
+
+
+#variance between/within (panel)
+
